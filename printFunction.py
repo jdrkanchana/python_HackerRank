1841ce046d741a22844e0df923191a59a92e839a
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-12 15:16:55 +0530

    Update printFunction.py

--- printFunction.py
+++ printFunction.py
@@ -2,3 +2,8 @@
 n = int(input().strip())
 for i in range(1,n+1):
     print(i, end = '')
+    #here the print happens on the same line, this method valid for python 3
+    #example output if n is 3, output=123
+    #output is not 1
+    #2
+    #3
