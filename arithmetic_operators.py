9d70224f7cc2813942df326c74300a36281771f0
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-12 14:14:11 +0530

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    output_one=a+b
+    output_two=a-b
+    output_three=a*b
+    print(output_one)
+    print(output_two)
+    print(output_three)
