c72123ecfc8eb884a87561206ff1ab02d1702aee
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-12 15:08:55 +0530

    Create float_and_int.py

---
+++ float_and_int.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    float_div=a/b#even though both a,b ints, result is float, in c int,divide by int result in int
+    print(int(float_div))
+    print(float_div)
