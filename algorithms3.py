5cd64f09efe45c1dd3525bf723b38771f7a56a13
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-11-11 11:00:48 +0530

    Update algorithms3.py

--- algorithms3.py
+++ algorithms3.py
@@ -1,3 +1,9 @@
+#alice and bob are two students
+#we enter alice marks as an array,enter bob marks as array, in respective consective order as per subject
+#when for same subject if alice scores higher than bob,alice gets one mark
+#when for same subject if bob scores higher than alice, bob gets one mark
+#when both score same mark no score for both alice n bob
+#print the final score alice and bob gets
 def sumOfArray(alice_ar,bob_ar):
     alice_sum=0
     bob_sum=0
@@ -15,4 +21,5 @@ def sumOfArray(alice_ar,bob_ar):
 alice_ar = list(map(int, input().rstrip().split()))
 bob_ar = list(map(int, input().rstrip().split()))
 result=sumOfArray(alice_ar,bob_ar)
+#here printing the array gives wrong display in array form, therefor print respectively the elements in array
 print(result[0],result[1])
