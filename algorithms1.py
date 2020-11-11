9304816472594378410ed1b8ba2f07eea31855db
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-11-11 10:07:39 +0530

    Create algorithms1.py

---
+++ algorithms1.py
@@ -0,0 +1,12 @@
+# A function to compute sum of two numbers and return it
+
+
+def solveMeFirst(a,b):
+    sum=a+b
+    return sum
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
